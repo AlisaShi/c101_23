06d827eebd0ad532669983de9cd8fca14a630632
AlisaShi <59513354+AlisaShi@users.noreply.github.com>
2023-04-13 14:44:49 +0800

    Update main.c

--- LAB#6/main.c
+++ LAB#6/main.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
 
 int multiplication(int i,int j){
-  
-  if(j==9){
-      printf("%d*%d=%d ",i,j,i*j);
-      
-    //  multiplication(i+1, j=9);
+  if(i!=10){
+    if(j==9){
+      printf("%d*%d=%d \n",i,j,i*j);
+      //printf("Hello World\n");
+      j=1;
+      multiplication(i+1, j);  
     }
     else{
-      multiplication(i, j+1);
       printf("%d*%d=%d ",i,j,i*j);
+      multiplication(i, j+1);
+      
     }
+    j=1;
+  //multiplication(i, j);
+    i=i+1;
+  }
+  
 
 }
 
 int main(void) {
-  printf("Hello World\n");
+  
   multiplication(1,1);
   return 0;
 }
