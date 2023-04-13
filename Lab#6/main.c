750cb3d869aafa62d6a079ea151b8894e403bfde
AlisaShi <59513354+AlisaShi@users.noreply.github.com>
2023-04-13 14:45:19 +0800

    Create main.c

---
+++ Lab#6/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int multiplication(int i,int j){
+  if(i!=10){
+    if(j==9){
+      printf("%d*%d=%d \n",i,j,i*j);
+      //printf("Hello World\n");
+      j=1;
+      multiplication(i+1, j);  
+    }
+    else{
+      printf("%d*%d=%d ",i,j,i*j);
+      multiplication(i, j+1);
+      
+    }
+    j=1;
+  //multiplication(i, j);
+    i=i+1;
+  }
+  
+
+}
+
+int main(void) {
+  
+  multiplication(1,1);
+  return 0;
+}
